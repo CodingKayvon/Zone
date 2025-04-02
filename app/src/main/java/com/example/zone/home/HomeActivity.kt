51fa8a0e65e5b56a0410c2d8b1b2ed6f49c59030
Kayvon Washington <washingtonkayvon@aol.com>
2025-04-02 13:45:49 -0700

    Initial commit

---
+++ app/src/main/java/com/example/zone/home/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.zone.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zone.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+
+    }
+}
