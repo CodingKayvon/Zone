5d84cb02d22d9416c9887b3b8f4c1126322de9f8
Kayvon Washington <washingtonkayvon@aol.com>
2025-04-03 18:21:12 -0700

    Changes to SignUp. Adds user to firebase.

--- app/src/main/java/com/example/zone/login/SignInActivity.kt
+++ app/src/main/java/com/example/zone/login/SignInActivity.kt
@@ -1,6 +1,8 @@
 package com.example.zone.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +14,15 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
+
+        //Connect Sign Up Screen
+        val signUpLink = findViewById<TextView>(R.id.siToSignUp)
+
+        //On Sign-Up Click || Change Screens
+        signUpLink.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
