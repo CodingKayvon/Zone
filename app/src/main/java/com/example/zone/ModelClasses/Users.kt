fb7b9a08c9b2fe07c5a0f6f2b3b0abdaad53fb98
Connor Penn <147745933+Duke507@users.noreply.github.com>
2025-05-04 00:04:08 -0700

    user adapter start

---
+++ app/src/main/java/com/example/zone/ModelClasses/Users.kt
@@ -0,0 +1,18 @@
+package com.example.zone.ModelClasses
+
+class Users {
+    private var uid: String = ""
+    private var username: String = ""
+    private var profile: String = ""
+    private var status: String = ""
+    private var search: String = ""
+
+    constructor(
+        uid: String,
+        username: String,
+        profile: String,
+        status: String,
+        search: String
+    )
+}
+
