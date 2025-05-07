a295e396cfe545ab325bfe2ad559e0cf9c3f4486
Connor Penn <147745933+Duke507@users.noreply.github.com>
2025-05-06 18:27:10 -0700

    more fix items

--- app/src/main/java/com/example/zone/home/friendfindingFragment.kt
+++ app/src/main/java/com/example/zone/home/friendfindingFragment.kt
@@ -1,14 +1,19 @@
 package com.example.zone.home
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.zone.AdapterClasses.UserAdapter
 import com.example.zone.ModelClasses.Users
 import com.example.zone.R
+import com.google.api.Distribution.BucketOptions.Linear
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -16,9 +21,12 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
+
 class friendfindingFragment : Fragment() {
     private var userAdapter: UserAdapter? = null
     private var mUsers: List<Users>? = null
+    private var recyclerView: RecyclerView? = null
+    private var searchEditText: EditText? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +35,28 @@ class friendfindingFragment : Fragment() {
 
         val view: View = inflater.inflate(R.layout.fragment_friendfinding, container, false)
 
+        recyclerView = view.findViewById(R.id.searchList)
+        recyclerView!!.setHasFixedSize(true)
+        recyclerView!!.layoutManager = LinearLayoutManager(context)
+
         mUsers = ArrayList()
         retrieveAllUsers()
 
+        searchEditText = view.findViewById<EditText>(R.id.searchUsersET)
+
+        searchEditText!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchForUsers(s.toString().toLowerCase())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+        })
         return view
         //add functionality to the button when you get the chance, i left some working functions for it
 
@@ -75,6 +102,7 @@ class friendfindingFragment : Fragment() {
                     }
                 }
                 userAdapter = UserAdapter(context!!, mUsers!!, false)
+                recyclerView!!.adapter = userAdapter
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -90,7 +118,23 @@ class friendfindingFragment : Fragment() {
             .startAt(str)
             .endAt(str + "\uf8ff")
 
-        queryUsers.addValueEventListener(object:ValueEventListener)
+        queryUsers.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                (mUsers as ArrayList<Users>).clear()
+                for (snapshot in p0.children) {
+                    val user: Users? = p0.getValue(Users::class.java)
+                    if (!(user!!.getUID()).equals(firebaseUserID)) {
+                        (mUsers as ArrayList<Users>).add(user)
+                    }
+                }
+                userAdapter = UserAdapter(context!!, mUsers!!, false)
+                recyclerView!!.adapter = userAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
 
         /*
         val queryUsers = FirebaseDatabase.getInstance().reference
